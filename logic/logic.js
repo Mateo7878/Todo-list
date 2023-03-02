c269d76acc8f0ac10593bfd7875adc40147a7e95
José Pablo Mateo Cárdenas <mateocardenaspablo@gmail.com>
2023-03-01 20:34:45 -0600

    Funcionalidad a boton agregar

--- logic/logic.js
+++ logic/logic.js
@@ -1,11 +1,24 @@
 const buttonAdd = document.querySelector("[data-form-btn]");
 
-
 const addTodo = (event) =>
 {
     event.preventDefault();
-    const buttonInput = document.querySelector("[data-form-input]");
-    console.log(buttonInput.value);
+    const Input = document.querySelector("[data-form-input]");
+    const value = Input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    Input.value = '';
+    const content = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+
+    list.appendChild(task);
+
+
 }
 
 
