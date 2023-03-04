16c573168a8933ccec398b8452013463e1e49f0d
José Pablo Mateo Cárdenas <mateocardenaspablo@gmail.com>
2023-03-04 08:58:08 -0600

    agregada funcion completar

--- logic/logic.js
+++ logic/logic.js
@@ -1,3 +1,4 @@
+( () => {
 const buttonAdd = document.querySelector("[data-form-btn]");
 
 const addTodo = (event) =>
@@ -9,17 +10,36 @@ const addTodo = (event) =>
     const task = document.createElement("li");
     task.classList.add("card");
     Input.value = '';
-    const content = ` <div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+
+
+    const content = `
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
-
+    task.appendChild(taskContent);
     list.appendChild(task);
+}
 
-
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", 'fa-check-square', "icon" );
+    i.addEventListener('click', compleTask);
+    return i;
 }
 
 
+const compleTask = (event) =>
+{
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+
+}
+
 buttonAdd.addEventListener('click', addTodo)
+})();
